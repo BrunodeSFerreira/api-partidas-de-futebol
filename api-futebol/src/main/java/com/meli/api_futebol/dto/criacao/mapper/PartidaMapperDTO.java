8ec6885622caa5a14ea087f72ab464765bcb1a8a
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-24 10:56:06 -0300

    Create PartidaMapperDTO

---
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/criacao/mapper/PartidaMapperDTO.java
@@ -0,0 +1,35 @@
+package com.meli.api_futebol.dto.criacao.mapper;
+
+import com.meli.api_futebol.dto.criacao.PartidaRequestDTO;
+import com.meli.api_futebol.model.ClubePartida;
+import com.meli.api_futebol.model.Estadio;
+import com.meli.api_futebol.model.EstadosBrasileiros;
+import com.meli.api_futebol.model.Partida;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PartidaMapperDTO {
+
+    public static Partida toEntity(PartidaRequestDTO dto, Estadio estadio) {
+
+        Partida partida = new Partida();
+        partida.setDataHora(dto.getDataHora());
+        partida.setEstadio(estadio);
+
+
+        List<ClubePartida> partidaDTO = partida.getClubesParticipates().stream().map(cp -> {
+            ClubePartida clubePartida = new ClubePartida();
+            clubePartida.setId(cp.getId());
+            clubePartida.setClube(cp.getClube());
+            clubePartida.setPartida(cp.getPartida());
+            clubePartida.setGols(cp.getGols());
+            clubePartida.setMandante(cp.isMandante());
+            return clubePartida;
+        }).collect(Collectors.toList());
+
+        partida.setClubesParticipates(partidaDTO);
+        return partida;
+    }
+
+}
