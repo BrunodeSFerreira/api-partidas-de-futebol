3cf628572ae305460f780372f969d8863deb7ef3
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-24 15:52:19 -0300

    Create PartidaReturnDTO

---
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/retorno/PartidaReturnDTO.java
@@ -0,0 +1,54 @@
+package com.meli.api_futebol.dto.retorno;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PartidaReturnDTO {
+
+    private Long id;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDateTime dataHora;
+    private Long estadioId;
+    private List<ClubePartidaReturnDTO> clubesParticipantes;
+
+    public PartidaReturnDTO(Long id, LocalDateTime dataHora, Long estadioId, List<ClubePartidaReturnDTO> clubesParticipantes) {
+        this.id = id;
+        this.dataHora = dataHora;
+        this.estadioId = estadioId;
+        this.clubesParticipantes = clubesParticipantes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Long getEstadioId() {
+        return estadioId;
+    }
+
+    public void setEstadioId(Long estadioId) {
+        this.estadioId = estadioId;
+    }
+
+    public List<ClubePartidaReturnDTO> getClubesParticipantes() {
+        return clubesParticipantes;
+    }
+
+    public void setClubesParticipantes(List<ClubePartidaReturnDTO> clubesParticipantes) {
+        this.clubesParticipantes = clubesParticipantes;
+    }
+}
