50543f56dcbce39c0820da1456b830670ebabf53
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-24 13:11:56 -0300

    Create EstadioMapperDTO

---
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/criacao/mapper/EstadioMapperDTO.java
@@ -0,0 +1,27 @@
+package com.meli.api_futebol.dto.criacao.mapper;
+
+import com.meli.api_futebol.dto.criacao.EstadioRequestDTO;
+import com.meli.api_futebol.model.Estadio;
+import com.meli.api_futebol.model.Partida;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EstadioMapperDTO {
+
+    public static Estadio toEntity(EstadioRequestDTO dto) {
+        Estadio estadio = new Estadio();
+        estadio.setNomeEstadio(dto.getNomeEstadio());
+
+        List<Partida> partidasDTO = estadio.getPartidas().stream().map(partidas -> {
+            Partida partida = new Partida();
+            partida.setDataHora(partidas.getDataHora());
+            partida.setEstadio(partidas.getEstadio());
+            partida.setClubesParticipates(partidas.getClubesParticipates());
+            return partida;
+        }).collect(Collectors.toList());
+        estadio.setPartidas(partidasDTO);
+        return estadio;
+    }
+
+}
