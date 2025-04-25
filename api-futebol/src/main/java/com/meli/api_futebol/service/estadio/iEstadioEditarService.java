ef4afc449d348fde16627db33c67c9d1fb550e7b
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-25 14:02:53 -0300

    create iEstadioEditarService interface

---
+++ api-futebol/src/main/java/com/meli/api_futebol/service/estadio/iEstadioEditarService.java
@@ -0,0 +1,11 @@
+package com.meli.api_futebol.service.estadio;
+
+import com.meli.api_futebol.model.Estadio;
+
+public interface iEstadioEditarService {
+
+    Estadio editarNomeEstadio(Long id, Estadio estadio);
+
+
+
+}
