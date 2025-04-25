edf2670585b78f729b487dc7fa2976b8f3bf92cf
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-25 13:53:48 -0300

    create iEstadioCadastroService interface

---
+++ api-futebol/src/main/java/com/meli/api_futebol/service/estadio/iEstadioCadastroService.java
@@ -0,0 +1,9 @@
+package com.meli.api_futebol.service.estadio;
+
+import com.meli.api_futebol.model.Estadio;
+
+public interface iEstadioCadastroService {
+
+    Estadio salvar(Estadio estadio);
+
+}
