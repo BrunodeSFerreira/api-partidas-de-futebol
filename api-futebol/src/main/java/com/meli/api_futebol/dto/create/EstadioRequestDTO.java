7a0d391db061a140c595061a4cd6b499953f4017
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-25 16:56:49 -0300

    remove annotations in EstadioRequestDTO

--- api-futebol/src/main/java/com/meli/api_futebol/dto/create/EstadioRequestDTO.java
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/create/EstadioRequestDTO.java
@@ -1,21 +1,12 @@
 package com.meli.api_futebol.dto.create;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.List;
 
 public class EstadioRequestDTO {
 
-    @NotBlank(message = "O nome do Estádio não pode ser nulo/branco.")
-    @Min(value = 3, message = "O nome deve contar pelo menos três caracteres.")
     private String nomeEstadio;
-    @NotNull(message = "Necessário informar o número identificador do Estádio.")
-    private List<PartidaRequestDTO> partidas;
 
-    public EstadioRequestDTO(String nomeEstadio, List<PartidaRequestDTO> partidas) {
+    public EstadioRequestDTO(String nomeEstadio) {
         this.nomeEstadio = nomeEstadio;
-        this.partidas = partidas;
+
     }
 
     public EstadioRequestDTO() {
@@ -29,11 +20,4 @@ public class EstadioRequestDTO {
         this.nomeEstadio = nomeEstadio;
     }
 
-    public List<PartidaRequestDTO> getPartidas() {
-        return partidas;
-    }
-
-    public void setPartidas(List<PartidaRequestDTO> partidas) {
-        this.partidas = partidas;
-    }
 }
