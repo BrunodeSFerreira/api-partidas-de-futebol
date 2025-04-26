78201129ef260e5be8d0c901193fb3bc5d2a6f3a
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-26 16:58:13 -0300

    Update iEstadioEditarService

--- api-futebol/src/main/java/com/meli/api_futebol/service/estadio/iEstadioEditarService.java
+++ api-futebol/src/main/java/com/meli/api_futebol/service/estadio/iEstadioEditarService.java
@@ -1,5 +1,6 @@
 package com.meli.api_futebol.service.estadio;
 
+import com.meli.api_futebol.dto.create.EstadioRequestDTO;
 import com.meli.api_futebol.model.Estadio;
 
 public interface iEstadioEditarService {
