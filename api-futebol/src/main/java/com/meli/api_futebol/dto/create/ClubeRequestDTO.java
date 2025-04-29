e09e8c29da03d3641188a8def10dafa2b2973f87
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-29 10:18:24 -0300

    Update ClubeRequestDTO

--- api-futebol/src/main/java/com/meli/api_futebol/dto/create/ClubeRequestDTO.java
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/create/ClubeRequestDTO.java
@@ -1,36 +1,26 @@
 package com.meli.api_futebol.dto.create;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.meli.api_futebol.model.EstadosBrasileiros;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import org.springframework.format.annotation.DateTimeFormat;
+
 
 import java.time.LocalDate;
-import java.util.List;
 
 public class ClubeRequestDTO {
 
-    @NotNull(message = "Nome do Clube não pode ser nulo.")
-    @Size(min = 2, message = "O nome do Clube não pode ter menos que dois caracteres.")
     private String nomeClube;
-
-    @NotNull(message = "É necessário informar o Estado de origem do clube.")
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private EstadosBrasileiros estadosBrasileiros;
-
-    @DateTimeFormat(pattern = "dd/MM/yyyy")
-    @NotNull(message = "Necessário informar data de criação do Clube.")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCriacao;
-
-    private List<ClubePartidaRequestDTO> clubePartidas;
 
     public ClubeRequestDTO() {
     }
 
-    public ClubeRequestDTO(String nomeClube, EstadosBrasileiros estadosBrasileiros, LocalDate dataCriacao, List<ClubePartidaRequestDTO> clubePartidas) {
+    public ClubeRequestDTO(String nomeClube, EstadosBrasileiros estadosBrasileiros, LocalDate dataCriacao) {
         this.nomeClube = nomeClube;
         this.estadosBrasileiros = estadosBrasileiros;
         this.dataCriacao = dataCriacao;
-        this.clubePartidas = clubePartidas;
+
     }
 
     public String getNomeClube() {
@@ -58,11 +48,4 @@ public class ClubeRequestDTO {
     }
 
 
-    public List<ClubePartidaRequestDTO> getClubePartidas() {
-        return clubePartidas;
-    }
-
-    public void setClubePartidas(List<ClubePartidaRequestDTO> clubePartidas) {
-        this.clubePartidas = clubePartidas;
-    }
 }
