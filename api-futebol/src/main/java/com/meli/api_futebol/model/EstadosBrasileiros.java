dc995f8389d5fc467bb07213e2fc7ea238757c7a
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-29 10:20:58 -0300

    Update EstadosBrasileiros enum

--- api-futebol/src/main/java/com/meli/api_futebol/model/EstadosBrasileiros.java
+++ api-futebol/src/main/java/com/meli/api_futebol/model/EstadosBrasileiros.java
@@ -2,43 +2,41 @@ package com.meli.api_futebol.model;
 
 public enum EstadosBrasileiros {
 
-    AC("AC", "Acre", 12),
-    AL("AL", "Alagoas", 27),
-    AM("AM", "Amazonas", 13),
-    AP("AP", "Amapá", 16),
-    BA("BA", "Bahia", 29),
-    CE("CE", "Ceará", 23),
-    DF("DF", "Distrito Federal", 53),
-    ES("ES", "Espírito Santo", 32),
-    GO("GO", "Goiás", 52),
-    MA("MA", "Maranhão", 21),
-    MG("MG", "Minas Gerais", 31),
-    MS("MS", "Mato Grosso do Sul", 50),
-    MT("MT", "Mato Grosso", 51),
-    PA("PA", "Pará", 15),
-    PB("PB", "Paraíba", 25),
-    PE("PE", "Pernambuco", 26),
-    PI("PI", "Piauí", 22),
-    PR("PR", "Paraná", 41),
-    RJ("RJ", "Rio de Janeiro", 33),
-    RN("RN", "Rio Grande do Norte", 24),
-    RO("RO", "Rondônia", 11),
-    RR("RR", "Roraima", 14),
-    RS("RS", "Rio Grande do Sul", 43),
-    SC("SC", "Santa Catarina", 42),
-    SE("SE", "Sergipe", 28),
-    SP("SP", "São Paulo", 35),
-    TO("TO", "Tocantins", 17);
+    AC("AC", "Acre"),
+    AL("AL", "Alagoas"),
+    AM("AM", "Amazonas"),
+    AP("AP", "Amapá"),
+    BA("BA", "Bahia"),
+    CE("CE", "Ceará"),
+    DF("DF", "Distrito Federal"),
+    ES("ES", "Espírito Santo"),
+    GO("GO", "Goiás"),
+    MA("MA", "Maranhão"),
+    MG("MG", "Minas Gerais"),
+    MS("MS", "Mato Grosso do Sul"),
+    MT("MT", "Mato Grosso"),
+    PA("PA", "Pará"),
+    PB("PB", "Paraíba"),
+    PE("PE", "Pernambuco"),
+    PI("PI", "Piauí"),
+    PR("PR", "Paraná"),
+    RJ("RJ", "Rio de Janeiro"),
+    RN("RN", "Rio Grande do Norte"),
+    RO("RO", "Rondônia"),
+    RR("RR", "Roraima"),
+    RS("RS", "Rio Grande do Sul"),
+    SC("SC", "Santa Catarina"),
+    SE("SE", "Sergipe"),
+    SP("SP", "São Paulo"),
+    TO("TO", "Tocantins");
 
 
     private final String sigla;
     private final String nome;
-    private final int codigo;
 
-    EstadosBrasileiros(String sigla, String nome, int codigo) {
+    EstadosBrasileiros(String sigla, String nome) {
         this.sigla = sigla;
         this.nome = nome;
-        this.codigo = codigo;
     }
 
     public String getSigla() {
@@ -49,7 +47,5 @@ public enum EstadosBrasileiros {
         return nome;
     }
 
-    public int getCodigo() {
-        return codigo;
-    }
+
 }
