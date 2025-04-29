f2bc6e49d0081b76343427c2f89039a8978190bf
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-29 10:21:43 -0300

    Update ClubeRepository

--- api-futebol/src/main/java/com/meli/api_futebol/repository/ClubeRepository.java
+++ api-futebol/src/main/java/com/meli/api_futebol/repository/ClubeRepository.java
@@ -1,9 +1,13 @@
 package com.meli.api_futebol.repository;
 
 import com.meli.api_futebol.model.Clube;
+import com.meli.api_futebol.model.EstadosBrasileiros;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClubeRepository extends JpaRepository<Clube, Long> {
+
 }
