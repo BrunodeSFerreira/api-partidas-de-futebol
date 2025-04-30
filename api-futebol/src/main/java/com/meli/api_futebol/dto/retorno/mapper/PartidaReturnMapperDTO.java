c08000e2c56ffe45ab30b76274e6a394b202ca90
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-30 11:03:16 -0300

    update PartidaReturnMapperDTO

--- api-futebol/src/main/java/com/meli/api_futebol/dto/retorno/mapper/PartidaReturnMapperDTO.java
+++ api-futebol/src/main/java/com/meli/api_futebol/dto/retorno/mapper/PartidaReturnMapperDTO.java
@@ -1,4 +1,5 @@
 package com.meli.api_futebol.dto.retorno.mapper;
+
 import com.meli.api_futebol.dto.retorno.ClubePartidaReturnDTO;
 import com.meli.api_futebol.dto.retorno.PartidaReturnDTO;
 import com.meli.api_futebol.model.Partida;
@@ -14,21 +15,21 @@ public class PartidaReturnMapperDTO {
         partidaReturnDTO.setDataHora(partida.getDataHora());
         partidaReturnDTO.setEstadioId(partida.getEstadio().getId());
 
-        List<ClubePartidaReturnDTO> clubePartidaReturnDTO = partida.getClubesParticipates().stream().map(cp ->
-                {
+        List<ClubePartidaReturnDTO> clubePartidaReturnDTO = partida.getClubesParticipates().stream()
+                .map(cp -> {
                     ClubePartidaReturnDTO clubePartidaReturn = new ClubePartidaReturnDTO();
+
                     clubePartidaReturn.setClubeId(cp.getId().getClubeId());
                     clubePartidaReturn.setPartidaId(cp.getId().getPartidaId());
                     clubePartidaReturn.setGols(cp.getGols());
                     clubePartidaReturn.setMandante(cp.isMandante());
 
                     return clubePartidaReturn;
-
-                }).collect(Collectors.toList());
+                })
+                .collect(Collectors.toList());
 
         partidaReturnDTO.setClubesParticipantes(clubePartidaReturnDTO);
 
         return partidaReturnDTO;
     }
-
 }
