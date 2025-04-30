3029d598b8f3e2b2d80dce140aa6342d0a2be1c5
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-04-30 11:00:05 -0300

    add news finders in ClubeRepository

--- api-futebol/src/main/java/com/meli/api_futebol/repository/ClubeRepository.java
+++ api-futebol/src/main/java/com/meli/api_futebol/repository/ClubeRepository.java
@@ -1,13 +1,24 @@
 package com.meli.api_futebol.repository;
 
+import com.meli.api_futebol.dto.retorno.ClubeReturnDTO;
 import com.meli.api_futebol.model.Clube;
 import com.meli.api_futebol.model.EstadosBrasileiros;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClubeRepository extends JpaRepository<Clube, Long> {
+
+    Page<Clube> findByStatusAtividadeTrue(Pageable pageable);
+    Page<Clube> findByNomeClubeContainingIgnoreCaseAndStatusAtividadeTrue (String nome, Pageable pageable);
+
+    Page<Clube> findByEstadosBrasileirosAndStatusAtividadeTrue(EstadosBrasileiros estado, Pageable pageable);
+
+    Page<Clube> findByStatusAtividade(boolean status, Pageable pageable);
 
 }
