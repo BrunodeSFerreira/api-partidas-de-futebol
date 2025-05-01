d78ea2dddd9571f14c1f95e389932a894c074eeb
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-05-01 14:34:36 -0300

    Update in Partida class with new notations

--- api-futebol/src/main/java/com/meli/api_futebol/model/Partida.java
+++ api-futebol/src/main/java/com/meli/api_futebol/model/Partida.java
@@ -1,19 +1,27 @@
 package com.meli.api_futebol.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
 @Data
 @Table
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Partida {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private LocalDateTime dataHora;
 
@@ -21,8 +29,8 @@ public class Partida {
     @JoinColumn(name = "estadioId")
     private Estadio estadio;
 
-
-    @OneToMany(mappedBy = "partida")
+    @JsonManagedReference
+    @OneToMany(mappedBy = "partida", cascade = CascadeType.ALL)
     private List<ClubePartida> clubesParticipates;
 
     public Long getId() {
