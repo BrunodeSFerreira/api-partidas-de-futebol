14d9ab9a26a030ecf662a82295424e2f1edebf11
Bruno Ferreira <bruno.dferreira@mercadolivre.com>
2025-05-01 14:32:58 -0300

    Update in ClubePartidaRepository

--- api-futebol/src/main/java/com/meli/api_futebol/repository/ClubePartidaRepository.java
+++ api-futebol/src/main/java/com/meli/api_futebol/repository/ClubePartidaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClubePartidaRepository extends JpaRepository<ClubePartida, ClubePartidaId> {
+public interface ClubePartidaRepository extends JpaRepository<ClubePartida, Long> {
 }
